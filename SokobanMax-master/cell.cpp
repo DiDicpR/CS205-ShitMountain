9295582035d76f7dce59408dbe174e210339badd
DiDicpR <106336303+DiDicpR@users.noreply.github.com>
2024-01-19 18:03:49 +0800

    add moveRIght

--- SokobanMax-master/cell.cpp
+++ SokobanMax-master/cell.cpp
@@ -28,8 +28,81 @@ cell::cell(cell::cell_type type){
 
 //当前位置往右移动
 bool cell::moveRight(int cur_row, int cur_col){
+    if (this->type==wall)
+    {
+        return false;
+    }
+    
+    if(cur_col==col-1){
+       
+        int new_col = 0;
+        int new_row = 0;
+        cell father = *this->father;
+        //在FATHER 的cells中将this找到，并确定对应的位置，记为new_row, new_col，给我代码
+        for(int i=0;i<father.row;i++){
+            for(int j=0;j<father.col;j++){
+                if(&father.cells[i][j]==this){
+                    new_row = i;
+                    new_col = j;
+                }
+            }
+        }
+        if (father.moveRight(new_row,new_col+1))
+        {
+            father.cells[new_row][new_col+1]=(*this);
+            this->father=father.father;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     cell temp=cells[cur_row][cur_col+1];
     cells[cur_row][cur_col+1]=cells[cur_row][cur_col];
-    cells[cur_row][cur_col]=new cell(empty);
-    
+    cells[cur_row][cur_col]=cell(empty);
+    if (temp.type==empty)
+    {
+        return true;
+    }
+    if (temp.type==wall)
+    {
+        cells[cur_row][cur_col]=cells[cur_row][cur_col+1];
+        cells[cur_row][cur_col+1]=temp;
+        return false;
+    }
+    if (this->moveRight(cur_row,cur_col+1))
+    {
+        return true;
+    }
+    else
+    {
+        cells[cur_row][cur_col]=cells[cur_row][cur_col+1];
+        cells[cur_row][cur_col+1]=temp;
+        return false;
+    }
+    if (temp.type==in_box)
+    {
+        if (this->moveRight(cur_row,cur_col+1))
+        {
+            return true;
+        }
+        else
+        {
+            if (temp.moveRight(cur_row/2,0))
+            {
+                temp.cells[cur_row/2][0]=(*this);
+                cells[cur_row][cur_col+1]=temp;
+                return true;
+            }else{
+                cells[cur_row][cur_col]=cells[cur_row][cur_col+1];
+                cells[cur_row][cur_col+1]=temp;
+                return false;
+
+            }
+        }
+    }
 }
+
+bool cell::
